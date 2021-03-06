fcb819aab56bbd95bdc059d78539752297971035
Jonathan.ybarra <jonatha.ybarra@gmail.com>
2021-03-06 17:02:59 -0300

    crud

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { RecoilRoot } from "recoil";
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
+
+function App() {
+  return (
+    <RecoilRoot>
+      <div className="App tc">
+        <h1 className="">
+          Todos{" "}
+          <span role="img" aria-label="flag">
+            🏁
+          </span>
+        </h1>
+        <div className="list pt3 br3">
+          <Todos />
+          <AddTodo />
+        </div>
+      </div>
+    </RecoilRoot>
+  );
+}
+
+export default App;
